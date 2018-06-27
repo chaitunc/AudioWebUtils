f67a908ab16f59b0682d9fb06c74d5c7334623de
Krishna Nallani <chaitu.nc@gmail.com>
2018-06-27 11:37:47 -0400

    test

--- app.py
+++ app.py
@@ -27,8 +27,24 @@ def init():
     url = os.environ.get('RABBITMQ_BIGWIG_RX_URL')
     params = pika.URLParameters(url)
     params.socket_timeout = 5
-    connection = pika.BlockingConnection(params) # Connect to CloudAMQP
-    channel = connection.channel()
+    connection = pika.SelectConnection(parameters=params,
+                                   on_open_callback=on_open)
+    try:
+        # Step #2 - Block on the IOLoop
+        connection.ioloop.start()
+        # Catch a Keyboard Interrupt to make sure that the connection is closed cleanly
+    except KeyboardInterrupt:
+        # Gracefully close the connection
+        connection.close()
+        # Start the IOLoop again so Pika can communicate, it will stop on its own when the connection is closed
+        connection.ioloop.start()
+
+# Step #3
+def on_open(connection):
+    connection.channel(on_channel_open)
+
+# Step #4
+def on_channel_open(channel):
     channel.queue_declare(queue='findSegments')
     channel.basic_consume(callback,
                       queue='findSegments',
