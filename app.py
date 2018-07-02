7bd80cafb2dfa58a4525654d8c93ae9652cff796
Krishna Nallani <chaitu.nc@gmail.com>
2018-07-02 13:43:53 -0400

    Queue Async test

--- app.py
+++ app.py
@@ -20,116 +20,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
-# Step #3
-def on_open(connection):
-    connection.channel(on_channel_open)
-
-# Step #4
-def on_channel_open(channel):
-    channel.queue_declare(callback2, 
-                          queue='findSegments')
-    channel.basic_consume(callback,
-                      queue='findSegments',
-                      no_ack=True)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    #channel.start_consuming()
-
-
-def callback2(channel):
-    print(" queue declared" )    
-    
-    
-def callback(ch, method, properties, body):
-    fileUrl = json.loads(body)
-    print(" [x] Received %r" % fileUrl)
-    fileId = fileUrl['id']
-    print(fileId)
-    accessToken = fileUrl['accessToken']
-    print("submitting asyn task")
-    credentials = AccessTokenCredentials(accessToken, 'my-user-agent/1.0')
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-    service = discovery.build('drive', 'v2', http=http)
-    url=service.files().get_media(fileId=fileId).execute()
-    print(len(url))
-    [Fs, x] = aIO.readAudioFileFromUrl(url)
-    ShortTermFeatures = aS.silenceRemoval(x, Fs, 0.020, 0.020, smoothWindow = 1.0, Weight = 0.3, plot = False)
-    [SVM, MEANSS, STDSS] = aS.step2(ShortTermFeatures)
-    MaxIdx = aS.step3(ShortTermFeatures, MEANSS, STDSS, SVM, 0.020, smoothWindow = 1.0, Weight = 0.3)
-    segments = aS.step4(MaxIdx,0.020)
-    sendConnection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='127.0.0.1'))
-    sendChannel = sendConnection.channel()
-    returnData = {}
-    returnData['segments'] = segments
-    returnData['id'] = fileId
-    message = json.dumps(returnData) 
-    sendChannel.queue_declare(callback2, 
-                              queue='getSegments')
-    
-    sendChannel.basic_publish(exchange='',
-                          routing_key='getSegments',
-                          body=message)
-    print(" [x] Sent  %r" % message)
-    sendConnection.close()
-    print(" [x] Received %r")
-
-        
-# Parse CLODUAMQP_URL (fallback to localhost)
-url = os.environ.get('RABBITMQ_BIGWIG_RX_URL')
-params = pika.URLParameters(url)
-params.socket_timeout = 5
-connection = pika.SelectConnection(parameters=params,
-                               on_open_callback=on_open)
-try:
-    # Step #2 - Block on the IOLoop
-    connection.ioloop.start()
-    # Catch a Keyboard Interrupt to make sure that the connection is closed cleanly
-except KeyboardInterrupt:
-    # Gracefully close the connection
-    connection.close()
-    # Start the IOLoop again so Pika can communicate, it will stop on its own when the connection is closed
-    connection.ioloop.start()
-
-
-
-
-
-
 
 @app.route("/")
 def hello():
+     async_task = AsyncTask()
+     async_task.start()
      return "Hello World!"
-
-# endpoint to submitForSegments 
-@app.route("/submitForSegments", methods=["POST"])
-def submitForSegments():
-    print("received request submitForSegments")
-    fileUrl = request.get_json()
-    fileId = fileUrl['id']
-    print(fileId)
-    accessToken = fileUrl['accessToken']
-    print("submitting asyn task")
-    credentials = AccessTokenCredentials(accessToken, 'my-user-agent/1.0')
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-    service = discovery.build('drive', 'v2', http=http)
-    url=service.files().get_media(fileId=fileId).execute()
-    print(len(url))
-    async_task = AsyncTask(fileId, url)
-    async_task.start()
-    return jsonify("Submitted")
-
-@app.route("/getSegments", methods=["POST"])
-def getSegments():
-    print("received request getSegments")
-    fileUrl = request.get_json()
-    fileId = fileUrl['id']
-    outputFile = open(fileId,"r")
-    segments = outputFile.read()
-    outputFile.close()
-    print(segments)
-    return jsonify(results = segments) 
 
 
 if __name__ == '__main__':
--- asyncTask.py
+++ asyncTask.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+from tempfile import TemporaryFile, NamedTemporaryFile
 from flask import Flask, request, jsonify
 from apiclient import discovery
 from oauth2client import client
@@ -6,27 +8,97 @@ from oauth2client.file import Storage
 from io import BytesIO
 from oauth2client.client import AccessTokenCredentials
 from flask import stream_with_context, request, Response, json
-
 import audioBasicIO as aIO
 import audioSegmentation as aS 
 import httplib2
 import os
+import os.path
+import logging
+import pika
+from oauth2client.service_account import ServiceAccountCredentials
 
 import threading  
 class AsyncTask(threading.Thread):
-    def __init__(self, fileId, url):
+    def __init__(self):
         super(AsyncTask,self).__init__()
-        self.fileId = fileId
-        self.url = url
-    def run(self):
-        print(len(self.url))
-        [Fs, x] = aIO.readAudioFileFromUrl(self.url)
+    # Step #3
+    def on_open(self,connection):
+        connection.channel(self.on_channel_open)
+    
+    # Step #4
+    def on_channel_open(self,channel):
+        channel.queue_declare(self.callback2, 
+                              queue='findSegments2')
+        channel.basic_consume(self.callback,
+                          queue='findSegments2',
+                          no_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        #channel.start_consuming()
+    
+    
+    def callback2(self,channel):
+        print(" queue declared" )    
+        
+        
+    def callback(self,ch, method, properties, body):
+        fileUrl = json.loads(body)
+        print(" [x] Received %r" % fileUrl)
+        fileId = fileUrl['id']
+        print(fileId)
+        accessToken = fileUrl['accessToken']
+        print("submitting asyn task")
+        credentials = AccessTokenCredentials(accessToken, 'my-user-agent/1.0')
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        service = discovery.build('drive', 'v2', http=http)
+        url=service.files().get_media(fileId=fileId).execute()
+        print(len(url))
+        [Fs, x] = aIO.readAudioFileFromUrl(url)
         ShortTermFeatures = aS.silenceRemoval(x, Fs, 0.020, 0.020, smoothWindow = 1.0, Weight = 0.3, plot = False)
         [SVM, MEANSS, STDSS] = aS.step2(ShortTermFeatures)
         MaxIdx = aS.step3(ShortTermFeatures, MEANSS, STDSS, SVM, 0.020, smoothWindow = 1.0, Weight = 0.3)
         segments = aS.step4(MaxIdx,0.020)
-        print(segments)
-        outputFile = open(self.fileId,"w") 
-        outputFile.write(json.dumps(segments))
-        outputFile.close()
+        queueurl = os.environ.get('RABBITMQ_BIGWIG_TX_URL')
+        #queueurl = 'amqp://_KUhGa2L:tDLeVGqDJCtkQmm7OBsGbiDhLsgL8h8s@scared-cowslip-30.bigwig.lshift.net:10242/ULoe2nXhmn9Y'
+        params = pika.URLParameters(queueurl)
+        sendConnection = pika.BlockingConnection(params)
+        sendChannel = sendConnection.channel()
+        returnData = {}
+        returnData['segments'] = segments
+        returnData['id'] = fileId
+        message = json.dumps(returnData) 
+        #sendChannel.queue_declare(self.callback2, 
+        #                          queue='getSegments')
+        
+        sendChannel.basic_publish(exchange='',
+                              routing_key='getSegments',
+                              body=message)
+        print(" [x] Sent  %r" % message)
+        sendConnection.close()
+        print(" [x] Received %r")    
+    
+    def run(self):
+        # Parse CLODUAMQP_URL (fallback to localhost)
+        url = os.environ.get('RABBITMQ_BIGWIG_RX_URL')
+        #url = 'amqp://_KUhGa2L:tDLeVGqDJCtkQmm7OBsGbiDhLsgL8h8s@scared-cowslip-30.bigwig.lshift.net:10243/ULoe2nXhmn9Y'
+        params = pika.URLParameters(url)
+        params.socket_timeout = 5
+        connection = pika.SelectConnection(parameters=params,
+                                       on_open_callback=self.on_open)
+        try:
+            # Step #2 - Block on the IOLoop
+            connection.ioloop.start()
+            # Catch a Keyboard Interrupt to make sure that the connection is closed cleanly
+        except KeyboardInterrupt:
+            # Gracefully close the connection
+            connection.close()
+            # Start the IOLoop again so Pika can communicate, it will stop on its own when the connection is closed
+            connection.ioloop.start()
         return 
+
+
+
+        
+
+
+
